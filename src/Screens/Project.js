c54a8f7cb96124cf6c7683c585891cd3575fc58d
sennounisara <sara.sennouni1996@gmail.com>
2020-09-04 14:23:59 +0100

    commit

--- src/Screens/Project.js
+++ src/Screens/Project.js
@@ -4,6 +4,7 @@ import Col from "react-bootstrap/Col";
 import dynamics from '../Image/Dynamics.PNG';
 import geofit from '../Image/geofit.png'
 import pfe from '../Image/pfe.png';
+import smartfocus from '../Image/smartfocus.PNG';
 import './style.css'
 
 class Project extends React.Component {
@@ -16,21 +17,26 @@ class Project extends React.Component {
                         <Col> <h3>Projects</h3></Col>
                     </Row>
                     <Row>
-                        <Col lg={4} md={6}>
+                        <Col lg={3} md={6}>
                             <img className="zoom" src={geofit} width="230" height="130" />
                             <h6>Development of a decision information system for Pexiluis Africa
                                 management.</h6>
-                            <p>Electron / Angular, Spring, MySQL</p>
+                            <p>Desktop: Electron / Web: Angular, Spring / database: MySQL</p>
                         </Col>
-                        <Col lg={4} md={6}>
+                        <Col lg={3} md={6}>
                             <Image className="zoom" src={dynamics} width="230" height="130"/>
                             <h6>Development of a Transport Management System using Dynamics 365</h6>
                             <p>Dynamics CRM 365</p>
                         </Col>
-                        <Col lg={4} md={6}>
+                        <Col lg={3} md={6}>
                             <img className="zoom" src={pfe} width="230" height="130"/>
                             <h6>University project management</h6>
                             <p>Php, Bootstrap, MySQL</p>
+                        </Col>
+                        <Col lg={3} md={6}>
+                            <img className="zoom" src={smartfocus} width="230" height="130"/>
+                            <h6>Smart Focus</h6>
+                            <p>Web: ReactJs / Mobile: Flutter </p>
                         </Col>
                     </Row>
                 </Container>
